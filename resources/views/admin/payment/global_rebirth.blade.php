50bccf5691bd6e1b3dc743b5baf1ac9cb96dfbd6
jasminesharmie <jasminesharmie@gmail.com>
2025-09-05 14:39:15 +0530

    jasminesharmie git commit -am jasminesharmie

--- resources/views/admin/payment/global_rebirth.blade.php
+++ resources/views/admin/payment/global_rebirth.blade.php
@@ -1,96 +1,94 @@
 @extends('admin.layouts.app')
 @section('admin/content')
 
-<div class="page-wrapper">
-    <div class="page-content">
+<section class="content-header">
+    <div class="container-fluid d-flex justify-content-between align-items-center">
         <h3>{{ auth()->user()->name }} - {{ auth()->user()->user_name }}</h3>
-
         <div class="d-flex justify-content-between align-items-center mb-3">
             <h4 class="mb-0">Global Rebirth</h4>
         </div>
+    </div>
+</section>
 
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-2 row-cols-xl-4">
-                            @foreach($Userplans as $Userplan)
-                            <a data-toggle="modal" data-target="#modal-xl{{ $Userplan->id }}">
-                                <div class="col">
-                                    <div class="card radius-10">
-                                        <div class="card-body">
-                                            <div class="d-flex align-items-center">
-                                                <div>
-                                                    <p class="mb-0 text-white">{{ $Userplan->plan_name }}</p>
-                                                    <h4 class="my-1 text-white">{{ $Userplan->plan_amount }} $</h4>
-                                                </div>
-                                                <div class="widgets-icons bg-light-transparent text-white ms-auto"><i
-                                                        class="lni lni-users"></i></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
+            @foreach($Userplans as $Userplan)
+            <div class="col-lg-3 col-6">
+                <!-- Small box -->
+                <div class="small-box bg-primary">
+                    <div class="inner">
+                        <h4>{{ $Userplan->plan_amount }} $</h4>
+                        <p>{{ $Userplan->plan_name }}</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fas fa-users"></i>
+                    </div>
+                    <a href="#" data-toggle="modal" data-target="#modal-xl{{ $Userplan->id }}" class="small-box-footer">
+                        View Details <i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                </div>
+            </div>
 
-                            <div class="modal fade" id="modal-xl{{ $Userplan->id }}">
-                                <div class="modal-dialog modal-xl">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h4 class="modal-title">{{ $Userplan->plan_name }} Global Regain Income</h4>
-                                            <button type="button" class="btn btn-primary"
-                                                data-dismiss="modal">X</button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <table id="example1" class="table table-bordered table-striped">
-                                                <thead>
-                                                    <tr>
-                                                        <th>S.No</th>
-                                                        <th>Full Name</th>
-                                                        <th>User Name</th>
-                                                        <th>Income ($)</th>
-                                                        <th>Package Amount ($)</th>
-                                                        <th>Date</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    @php
-                                                    $getFTdata = DB::table('global_regain')
-                                                    ->select('users.name','users.user_name','global_regain.amount','global_regain.created_at','global_regain.plan_id',
-                                                    'global_regain.to_id')
-                                                    ->join('users','users.id','=','global_regain.from_id')
-                                                    ->where('global_regain.pay_reason_id', 2)
-                                                    ->where('global_regain.from_id', auth()->user()->id)
-                                                    ->where('global_regain.plan_id', $Userplan->id)->get();
-                                                    @endphp
-                                                    @foreach ($getFTdata as $key=> $data)
-                                                    @php
-                                                    $plan = DB::table('plans')->where('id',$data->plan_id)->first();
-                                                    $user = DB::table('users')->where('id',$data->to_id)->first();
-                                                    @endphp
+            <div class="modal fade" id="modal-xl{{ $Userplan->id }}">
+                <div class="modal-dialog modal-xl">
+                    <div class="modal-content">
+                        <div class="modal-header bg-primary">
+                            <h4 class="modal-title">
+                                {{ $Userplan->plan_name }} - Global Regain Income
+                            </h4>
+                            <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <table class="table table-bordered table-striped">
+                                <thead class="thead-dark">
+                                    <tr>
+                                        <th>S.No</th>
+                                        <th>Full Name</th>
+                                        <th>User Name</th>
+                                        <th>Income ($)</th>
+                                        <th>Package Amount ($)</th>
+                                        <th>Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @php
+                                    $getFTdata = DB::table('global_regain')
+                                    ->select('users.name','users.user_name','global_regain.amount',
+                                    'global_regain.created_at','global_regain.plan_id',
+                                    'global_regain.to_id')
+                                    ->join('users','users.id','=','global_regain.from_id')
+                                    ->where('global_regain.pay_reason_id', 2)
+                                    ->where('global_regain.from_id', auth()->user()->id)
+                                    ->where('global_regain.plan_id', $Userplan->id)
+                                    ->get();
+                                    @endphp
 
-                                                    <tr>
-                                                        <td>{{ $key + 1 }}</td>
-                                                        <td>{{ $user->name }}</td>
-                                                        <td>{{ $user->user_name }}</td>
-                                                        <td>{{ $data->amount }} $</td>
-                                                        <td>{{ $plan->plan_amount }} $</td>
-                                                        <td>{{ $data->created_at }}</td>
-                                                    </tr>
-                                                    @endforeach
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            @endforeach
-
+                                    @foreach ($getFTdata as $key => $data)
+                                    @php
+                                    $plan = DB::table('plans')->where('id',$data->plan_id)->first();
+                                    $user = DB::table('users')->where('id',$data->to_id)->first();
+                                    @endphp
+                                    <tr>
+                                        <td>{{ $key + 1 }}</td>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{ $user->user_name }}</td>
+                                        <td>{{ $data->amount }} $</td>
+                                        <td>{{ $plan->plan_amount }} $</td>
+                                        <td>{{ date('d M Y h:i A', strtotime($data->created_at)) }}</td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
             </div>
+            @endforeach
         </div>
     </div>
-
-    <script src="https://code.jquery.com/jquery-latest.min.js"></script>
-    @endsection
+</section>
+@endsection
--- resources/views/admin/payment/spornser.blade.php
+++ resources/views/admin/payment/spornser.blade.php
@@ -1,74 +1,82 @@
 @extends('admin.layouts.app')
 @section('admin/content')
-
-<div class="page-wrapper">
-    <div class="page-content">
+<section class="content-header">
+    <div class="container-fluid d-flex justify-content-between align-items-center">
         <h3>{{ auth()->user()->name }} - {{ auth()->user()->user_name }}</h3>
+        <h6 class="mb-0">Sponsor Income</h6>
+    </div>
+</section>
 
-        <div class="page-breadcrumb d-flex align-items-center justify-content-between mb-3">
-            <div class="mb-0">
-                <h6>Sponsor</h6>
+<!-- Content -->
+<section class="content">
+    <div class="container-fluid">
+        <div class="card mb-3">
+            <div class="card-body">
+                <form method="GET" action="{{ route('sponserlist') }}" class="form-inline">
+                    <div class="form-group mr-2">
+                        <label for="from" class="mr-2">From:</label>
+                        <input type="date" class="form-control" id="from" name="from" value="{{ $from }}">
+                    </div>
+                    <div class="form-group mr-2">
+                        <label for="to" class="mr-2">To:</label>
+                        <input type="date" class="form-control" id="to" name="to" value="{{ $to }}">
+                    </div>
+                    <button type="submit" class="btn btn-primary">
+                        <i class="fas fa-search"></i> Search
+                    </button>
+                </form>
             </div>
         </div>
-        <form method="GET" action="{{ route('sponserlist') }}" class="mb-3">
-            <div class="row g-2 align-items-center">
-                <div class="col-md-3">
-                    <input type="date" class="form-control" name="from" value="{{ $from }}">
-                </div>
 
-                <div class="col-md-3">
-                    <input type="date" class="form-control" name="to" value="{{ $to }}">
-                </div>
-
-                <div class="col-md-1">
-                    <button class="btn btn-primary"><i class="bx bx-search"></i></button>
-                </div>
+        <div class="card">
+            <div class="card-header bg-primary text-white">
+                <h5 class="card-title mb-0"><i class="fas fa-user-friends"></i> Sponsor Income List</h5>
             </div>
-        </form>
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table id="example2" class="table table-striped table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>From ID</th>
-                                        <th>From Name</th>
-                                        <th>Sponser Amount 50% ($)</th>
-                                        <th>Package Amount ($)</th>
-                                        <th>Reason</th>
-                                        <th>Date</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach($spornsers as $key => $spornser)
-                                    <tr>
-                                        <td>{{ ($spornsers->currentPage() - 1) * $spornsers->perPage() + $key + 1 }}
-                                        </td>
-                                        <td>{{ $spornser->from_username }}</td>
-                                        <td>{{ $spornser->from_name }}</td>
-                                        <td>{{ $spornser->amount }} $</td>
-                                        <td>{{ $spornser->plan_amount }} $</td>
-                                        <td>{{ $spornser->reason_name }}</td>
-                                        <td>{{ $spornser->created_at }}</td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table id="example2" class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>From ID</th>
+                                <th>From Name</th>
+                                <th>Sponsor Amount 50% ($)</th>
+                                <th>Package Amount ($)</th>
+                                <th>Reason</th>
+                                <th>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($spornsers as $key => $spornser)
+                            <tr>
+                                <td>{{ ($spornsers->currentPage() - 1) * $spornsers->perPage() + $key + 1 }}</td>
+                                <td>{{ $spornser->from_username }}</td>
+                                <td>{{ $spornser->from_name }}</td>
+                                <td>{{ $spornser->amount }} $</td>
+                                <td>{{ $spornser->plan_amount }} $</td>
+                                <td>{{ $spornser->reason_name }}</td>
+                                <td>{{ \Carbon\Carbon::parse($spornser->created_at)->format('d-m-Y H:i') }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</section>
+@endsection
 
-<script src="https://code.jquery.com/jquery-latest.min.js"></script>
+@push('page_scripts')
 <script>
-$(document).ready(function() {
-    $('#example2').DataTable()
+$(function() {
+    $('#example2').DataTable({
+        "paging": false,
+        "ordering": true,
+        "info": false,
+        "autoWidth": false,
+        "responsive": true
+    });
 });
 </script>
-@endsection
+@endpush
